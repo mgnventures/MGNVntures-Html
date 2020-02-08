c7e07155d964fe6e436f79192aea656afaa4e294
makslab <muhammadali_138@hotmail.com>
2020-02-09 04:06:05 +0500

    Removed comments and fixed content

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-/* ===================================================================
- * Transcend - Main JS
- *
- * ------------------------------------------------------------------- */
-
 (function($) {
 	
 	
@@ -355,16 +350,7 @@
             url: cfg.mailChimpURL
         });
 
-        // Mailchimp translation
-        //
-        //  Defaults:
-        //	 'submit': 'Submitting...',
-        //  0: 'We have sent you a confirmation email',
-        //  1: 'Please enter a value',
-        //  2: 'An email address must contain a single @',
-        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
-        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
-        //  5: 'This email address looks fake or invalid. Please enter a real email address'
+        
 
         $.ajaxChimp.translations.es = {
             'submit': 'Submitting...',
